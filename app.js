e87bf786ae4730f218a0a6055d3fe6ef97188bc3
Corey Norris <coreypnorris@gmail.com>
2015-11-27 12:37:31 -0800

    Add POST route to handle new notes

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 var express = require('express');
-var app = express();
-var mongoClient = require('mongodb').MongoClient;
-
-mongoClient.connect('mongodb://localhost:27017/node_mongo_app_template', function(err, db) {
-  if (err) {
-    throw err;
-  }
-});
-
 require('dotenv').load();
+
+var mongoose = require('mongoose');
+var noteModel = require('./models/note');
+mongoose.connect('mongodb://localhost:27017/node_mongo_app_template');
+
+var app = express();
 app.set('view engine', 'jade');
 app.use(express.static('public'));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/', function (req, res) {
   // NOTE: In Node.js, __dirname is always the directory in which the currently executing script resides.
   res.render(__dirname + '/public/views/home.jade',
     { apiKey: process.env.api_key}
   )
+})
+
+app.post('/notes', function (req, res) {
+  var newNote = new noteModel();
+  newNote.body = req.body.body;
+  newNote.save(function(err) {
+    if (err)
+      res.send(err);
+    res.redirect('/');
+  });
 })
 
 var server = app.listen(3000, function () {
